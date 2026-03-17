f29a0058986240fcc77e7d58c0e3c96ffec5714b
manan1000 <mananhemani29@gmail.com>
2026-03-17 18:09:17 +0530

    init

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = Bun.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`); 
+});
